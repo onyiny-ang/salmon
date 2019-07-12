13ec3120a2ca75b5f5a5c657e7e67b3bd2c26447
onyinyang <lt15bo@brocku.ca>
2019-07-11 21:55:34 -0400

    Change parameters for dna774

--- SalmonTSP/dna774/Global.h
+++ SalmonTSP/dna774/Global.h
@@ -2,15 +2,15 @@
 #define GLOBAL_H
 
 #define RESTARTS 1   // run the entire algorithm this many times
-#define GENERATIONS 1500
-#define SURVIVAL 55 // The number of salmon that survive to spawn each generation
-#define MEMORY .98   // The salmon will select this proportion of vertices from memory
-#define POPULATION 1500
+#define GENERATIONS 2000
+#define SURVIVAL 40 // The number of salmon that survive to spawn each generation
+#define MEMORY .99   // The salmon will select this proportion of vertices from memory
+#define POPULATION 2000
 #define BIG_NUMBER 8000000
 #define SMALL_NUMBER .00001
 #define BEST_KNOWN 7294621
 #define TOTAL_DNA 7740000
-#define BETA 1   // exponent for flow in roulette selection
+#define BETA .75   // exponent for flow in roulette selection
 #define ELITE 'Y'  // set to Y for elite salmon and N for no elite
 
 // file parameters
